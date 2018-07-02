1309392bd2e2eb97410f1a770f8183bb730e8088
A1an <songxz1992@gmail.com>
2018-07-03 06:36:19 +0800

    用协程启动多coinex账号交易成功，继续完善交易算法

--- app.py
+++ app.py
@@ -1,43 +1,68 @@
 import configparser
-import ccxt
-from pprint import pprint
+import asyncio
+import ccxt.async as ccxt
+from datetime import datetime
+import time
 
 
-# 读取配置信息
 def readUserConfig(configFile):
     config = configparser.ConfigParser()
     config.read(configFile, encoding='UTF-8')
     return config
 
 
-# 主程序
-if __name__ == '__main__':
-    userConfig = readUserConfig('config/apikeys.cfg')
-    apiKey1 = userConfig['COINEX1']['apiKey']
-    secretKey1 = userConfig['COINEX1']['secretKey']
-    apiKey2 = userConfig['COINEX2']['apiKey']
-    secretKey2 = userConfig['COINEX2']['secretKey']
-    if apiKey1 == '' or secretKey1 == '' or apiKey2 == '' or secretKey2 == '':
-        print('Lost value(s) in "config/apikeys.cfg"')
-        exit()
-    coinex1 = ccxt.coinex({
+async def updateMiningInfo(apiKey, secretKey):
+    coinex = ccxt.coinex({
         'api': {
             'private': {
-                'get': ['balance',
-                        'order',
-                        'order/pending',
-                        'order/finished',
-                        'order/finished/{id}',
-                        'order/user/deals',
-                        'order/mining/difficulty',
-                        ],
+                'get': [
+                    'balance',
+                    'order',
+                    'order/pending',
+                    'order/finished',
+                    'order/finished/{id}',
+                    'order/mining/difficulty',
+                ],
             },
         },
-        'apiKey': apiKey1,
-        'secret': secretKey1, })
-    coinex2 = ccxt.coinex({
-        'apiKey': apiKey2,
-        'secret': secretKey2, })
-    markets = coinex1.load_markets()
-    pprint(coinex1.api)
-    print(coinex1.private_get_order_mining_difficulty())
+        'apiKey': apiKey,
+        'secret': secretKey,
+    })
+
+    result = await coinex.private_get_order_mining_difficulty()
+    if result['code'] == 0:
+        global MINING_DIFF
+        MINING_DIFF = result['data']['difficulty']
+    else:
+        print(result['code'], result['message'])
+
+    await coinex.close()
+
+
+async def startTrading(apiKey, secretKey):
+    print(1, apiKey)
+    await asyncio.sleep(1)
+    print(2, secretKey)
+
+
+if __name__ == '__main__':
+
+    CURRENT_HOUR = None
+    MINING_DIFF = None
+
+    config = readUserConfig('config/apikeys.cfg')
+
+    loop = asyncio.get_event_loop()
+
+    while True:
+        current_hour = datetime.utcnow().hour
+        if CURRENT_HOUR != current_hour:
+            if current_hour == 0 or CURRENT_HOUR is None:
+                loop.run_until_complete(updateMiningInfo(config.defaults()['apikey'], config.defaults()['secretkey']))
+            CURRENT_HOUR = current_hour
+            tasks = [startTrading(config[option]['apikey'], config[option]['secretkey']) for option in
+                     config.sections()]
+            loop.run_until_complete(asyncio.gather(*tasks))
+        print(CURRENT_HOUR)
+        print(MINING_DIFF)
+        time.sleep(60)
