e5e5bfa47cf5fe4846e8726fbe57c36bb997f29a
A1an <songxz1992@gmail.com>
2018-07-03 16:07:09 +0800

    修复了拖单bug，在安全范围内加快了一点交易频率

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def readUserConfig(configFile):
 
 
 async def updateMiningInfo(apiKey, secretKey):
+    global CURRENT_HOUR
     global MINING_DIFF
     global CURRENT_CET_PRICE
     global TARGET_TRADING_FEE
@@ -40,6 +41,11 @@ async def updateMiningInfo(apiKey, secretKey):
     CURRENT_CET_PRICE = float('%.4f' % float(current_cet_price['last']))
     TARGET_TRADING_FEE = float('%.2f' % (CURRENT_CET_PRICE * MINING_DIFF))
 
+    print('当前UTC时间（取小时位）：', CURRENT_HOUR)
+    print('当前挖矿难度（CET/小时）', MINING_DIFF)
+    print('当前对标CET价格', CURRENT_CET_PRICE)
+    print('当前交易手续费目标（折算USDT）', TARGET_TRADING_FEE)
+
     await coinex.close()
 
 
@@ -53,7 +59,7 @@ async def startTrading(apiKey, secretKey):
     })
 
     balance = await coinex.fetch_balance()
-    cet_amount = float('%.2f' % balance['CET']['free'])
+    cet_amount = balance['CET']['free']
     if cet_amount > 1:
         await coinex.create_market_sell_order('CET/USDT', cet_amount)
 
@@ -72,16 +78,15 @@ async def startTrading(apiKey, secretKey):
         trading_price = float('%.4f' % ((sc_buy_price + sc_sell_price) / 2))
         print('trading_price:', trading_price)
 
-        if sc_amount * trading_price < usdt_amount * 0.9:
-            buy_sc_order = await coinex.create_market_buy_order('SC/USDT', usdt_amount * 0.4)
+        if sc_amount * trading_price < usdt_amount * 0.1:
+            buy_sc_order = await coinex.create_market_buy_order('SC/USDT', usdt_amount * 0.3)
             trading_fee_sum += buy_sc_order['fee']['cost'] * float(buy_sc_order['info']['avg_price'])
             print('计入市价买单手续费，trading_fee_sum', trading_fee_sum)
-        elif sc_amount * trading_price * 0.9 > usdt_amount:
-            sell_sc_order = await coinex.create_market_sell_order('SC/USDT', sc_amount * 0.4)
+        elif sc_amount * trading_price > usdt_amount * 0.9:
+            sell_sc_order = await coinex.create_market_sell_order('SC/USDT', sc_amount * 0.3)
             trading_fee_sum += sell_sc_order['fee']['cost']
             print('计入市价卖单手续费，trading_fee_sum', trading_fee_sum)
         else:
-            print('trading_price', trading_price)
             sell_order = await coinex.create_limit_sell_order('SC/USDT', sc_amount, trading_price)
             buy_order = await coinex.create_limit_buy_order('SC/USDT', sc_amount, trading_price)
             if sell_order['status'] == 'closed':
@@ -91,7 +96,10 @@ async def startTrading(apiKey, secretKey):
                 trading_fee_sum += buy_order['fee']['cost'] * trading_price
                 print('计入限价买单手续费，trading_fee_sum', trading_fee_sum)
 
-        time.sleep(1)
+        openedOrders = await coinex.fetch_open_orders('SC/USDT')
+        if len(openedOrders) > 2:
+            for order in openedOrders:
+                await coinex.cancel_order(order['id'], 'SC/USDT')
 
     openedOrders = await coinex.fetch_open_orders('SC/USDT')
     for order in openedOrders:
@@ -116,13 +124,9 @@ if __name__ == '__main__':
         current_hour = datetime.utcnow().hour
         if CURRENT_HOUR != current_hour:
             CONFIG = readUserConfig('config/apikeys.cfg')
-            loop.run_until_complete(updateMiningInfo(CONFIG.defaults()['apikey'], CONFIG.defaults()['secretkey']))
             CURRENT_HOUR = current_hour
+            loop.run_until_complete(updateMiningInfo(CONFIG.defaults()['apikey'], CONFIG.defaults()['secretkey']))
             tasks = [startTrading(CONFIG[option]['apikey'], CONFIG[option]['secretkey']) for option in
                      CONFIG.sections()]
             loop.run_until_complete(asyncio.gather(*tasks))
-        print('当前UTC时间（取小时位）：', CURRENT_HOUR)
-        print('当前挖矿难度（CET/小时）', MINING_DIFF)
-        print('当前对标CET价格', CURRENT_CET_PRICE)
-        print('当前交易手续费目标（折算USDT）', TARGET_TRADING_FEE)
         time.sleep(600)
